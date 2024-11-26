452ed5e81e175967ae2bbb91c4ba4b6ee8d6cedb
IT-3133 <thilankavishka25@gmail.com>
2024-11-26 14:51:16 +0530

    Project Initialization

---
+++ myapp/src/App.js
@@ -0,0 +1,31 @@
+import "./App.css";
+import { useState } from "react";
+import {BrowserRouter as Router,Routes,Route,Link,NavLink} from 'react-router-dom';
+import Aboutus from './components/Aboutus';
+import Contactus from './components/contactus';
+import Home from './components/Home';
+import Dashboard from './components/Dashboard';
+import Login from './components/Login';
+function App() {
+ 
+  return (
+    <>
+      
+         <Router> 
+            <Routes>
+              <Route path='/aboutus' element={<Aboutus/>}/>
+              <Route path='/contactus' element={<Contactus/>}/>
+              <Route path='/home' element={<Home/>}/>
+              <Route path='/dashboard' element={<Dashboard/>}/>
+              <Route path='/login' element={<Login/>}/>
+            </Routes>
+         </Router>
+
+
+      
+
+    </>
+  );
+}
+
+export default App;
